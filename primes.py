cbceb028e0a270edef10405f3396df5b3298a603
Alkhodir, Jana (k20061055) <jana.alkhodir@kcl.ac.uk>
2022-10-06 16:37:02 +0100

    Add primes method to generatet a list with prime numbers

--- primes.py
+++ primes.py
@@ -3,4 +3,17 @@
 
 def primes(number_of_primes):
     list = []
+    n = 2
+    i=0
+    while i < number_of_primes:
+        prime = True
+        for j in range(2, n) :
+            if n % j == 0:
+                prime = False
+
+        if prime:
+            list.append (n)
+            i += 1
+
+        n += 1
     return list
